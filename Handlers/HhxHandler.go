6561c0dcbbbbead53cc7c095f395d3ec399d6506
1350585339@qq.com <1350585339@qq.com>
2018-06-12 15:15:55 +0800

    gorm增删改查

--- Handlers/HhxHandler.go
+++ Handlers/HhxHandler.go
@@ -6,27 +6,52 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 	"MsgApi/Config"
-	"net/http"
 )
 
-type Hhx struct{
-	FirstName        string   `json:"first_name"`
-	LastName       string    `json:"last_name"`
+type Hht struct{
+	Id               int64      `json:"id"`
+	One        string   `json:"one"`
+	Two       string    `json:"two"`
+	Three       string    `json:"three"`
 }
-//func (Hhx) TableName() string {
-//	return "hhx"
-//}
+func (Hht) TableName() string {
+	return "hht"
+}
 func Get_hhx(context *gin.Context){
 	db, err := gorm.Open("mysql", Config.MSQ)
 	defer db.Close()
 	if err != nil {
 		log.Panic("mysql db connect faild --- " + err.Error())
 	}
-	hhx := Hhx{FirstName: "Xi",LastName: "Huang"}
-	db.Create(&hhx)
-	context.JSON(http.StatusOK,gin.H{
-		"code":200,
-		"email": hhx,
-	})
+
+	//var hht Hht
+	//1.取出第一个数据（正确）
+	//db.First(&hht).Scan(&hht)
+	//2.保存数据  ok
+	//var hht1 = Hht{One: "hebe", Two: "tien"}
+	//var hht2 = Hht{One: "ella", Two: "chen"}
+	//var hht3 = Hht{One: "selina", Two: "ren"}
+	//var hht4 = Hht{One: "anpu", Two: "jiao"}
+	//
+	//db.Create(&hht1)
+	//db.Create(&hht2)
+	//db.Create(&hht3)
+	//db.Create(&hht4)
+	//3.list数据
+	//var hht =[]Hht{}
+	//db.Where("id >?", 0).Find(&hht).Scan(&hht)
+	////fmt.Println(hhy)
+	//for _, hh := range hht {
+	//	context.JSON(http.StatusOK, gin.H{
+	//		"code": 200,
+	//		"One":  hh.One,
+	//		"two":  hh.Two,
+	//	})
+	//}
+	//return
+	//4.更改某一列
+	//db.Model(&hht).Update("three", "love")
+
+
 
 }
